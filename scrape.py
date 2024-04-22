e372577db7e5e1cbe234610d1ff515ff758246c5
Sai Ram <75354390+am-ram@users.noreply.github.com>
2024-04-22 11:01:15 +0530

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -6,11 +6,13 @@ app = Flask(__name__)
 @app.route('/scrape', methods=['GET','POST'])
 def scrape():
     base_url = 'https://www.flipkart.com/search?q=mobiles&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off'
+    user_agent = request.headers.get('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36')
+
     all_product_data = []
-    all_product_data += scrape_page(base_url)
+    all_product_data += scrape_page(base_url,user_agent)
 
     # Determine the number of pages to scrape
-    num_pages = 12 
+    num_pages = 3  
     for page in range(2, num_pages + 1):
         page_url = f"{base_url}&page={page}"
         all_product_data += scrape_page(page_url)
@@ -18,4 +20,4 @@ def scrape():
     return jsonify(all_product_data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000)
+    app.run(debug=True)
